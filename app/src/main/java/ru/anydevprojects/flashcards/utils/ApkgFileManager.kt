cd5d3da4af0f8d6f67dd1eb7dd63e52d598ad060
Nikolay <you@example.com>
2025-01-10 21:31:26 +0500

    Добавлен класс для работы с архивами apkg

---
+++ app/src/main/java/ru/anydevprojects/flashcards/utils/ApkgFileManager.kt
@@ -0,0 +1,23 @@
+package ru.anydevprojects.flashcards.utils
+
+import java.io.File
+import java.util.zip.ZipFile
+
+object ApkgFileManager {
+    fun extractFile(filePath: String, outputDir: String) {
+        val zipFile = ZipFile(filePath)
+        zipFile.entries().asSequence().forEach { entry ->
+            val file = File(outputDir, entry.name)
+            if (entry.isDirectory) {
+                file.mkdirs()
+            } else {
+                file.parentFile?.mkdirs()
+                zipFile.getInputStream(entry).use { input ->
+                    file.outputStream().use { output ->
+                        input.copyTo(output)
+                    }
+                }
+            }
+        }
+    }
+}
